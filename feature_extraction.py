c22fb1f16f5ec190cae1b607d4a49b60e55d52b6
Quan Wang <quanw@google.com>
2022-05-07 14:36:37 -0400

    implement feature extraction and triplet

---
+++ feature_extraction.py
@@ -0,0 +1,61 @@
+import librosa
+import soundfile as sf
+import os
+import glob
+import myconfig
+import random
+
+
+def extract_features(flac_file):
+    """Extract MFCC features from a flac file."""
+    waveform, sample_rate = sf.read(flac_file)
+    features = librosa.feature.mfcc(
+        y=waveform, sr=sample_rate, n_mfcc=myconfig.N_MFCC)
+    return features
+
+
+def get_spk_to_utts(data_dir):
+    """Get the dict from speaker to list of utterances."""
+    flac_files = glob.glob(os.path.join(data_dir, "*", "*", "*.flac"))
+    spk_to_utts = dict()
+    for flac_file in flac_files:
+        basename = os.path.basename(flac_file)
+        split_name = basename.split("-")
+        spk = split_name[0]
+        if spk not in spk_to_utts:
+            spk_to_utts[spk] = [flac_file]
+        else:
+            spk_to_utts[spk].append(flac_file)
+    return spk_to_utts
+
+
+def get_triplet(spk_to_utts):
+    """Get a triplet of anchor/pos/neg samples."""
+    pos_spk, neg_spk = random.sample(spk_to_utts.keys(), 2)
+    anchor_utt, pos_utt = random.sample(spk_to_utts[pos_spk], 2)
+    neg_utt = random.sample(spk_to_utts[neg_spk], 1)[0]
+    return (anchor_utt, pos_utt, neg_utt)
+
+
+def get_triplet_features(spk_to_utts):
+    """Get a triplet of anchor/pos/neg features."""
+    anchor_utt, pos_utt, neg_utt = get_triplet(spk_to_utts)
+    return (extract_features(anchor_utt),
+            extract_features(pos_utt),
+            extract_features(neg_utt))
+
+
+def main():
+    features = extract_features(os.path.join(
+        myconfig.TEST_DATA_DIR, "61/70968/61-70968-0000.flac"))
+    print("Shape of features:", features.shape)
+
+    spk_to_utts = get_spk_to_utts(myconfig.TEST_DATA_DIR)
+    triplet = get_triplet(spk_to_utts)
+    triplet_features = get_triplet_features(spk_to_utts)
+    print(triplet)
+    print(triplet_features)
+
+
+if __name__ == "__main__":
+    main()
---
+++ myconfig.py
@@ -0,0 +1,8 @@
+# This file has the configurations of the experiments.
+
+# Path of downloaded LibriSpeech datasets.
+TRAIN_DATA_DIR = "/home/quan/Code/github/SpeakerRecognitionFromScratch/data/LibriSpeech/train-clean-100"
+TEST_DATA_DIR = "/home/quan/Code/github/SpeakerRecognitionFromScratch/data/LibriSpeech/test-clean"
+
+# Number of MFCCs for librosa.feature.mfcc.
+N_MFCC = 40
